246ecc122d8dd6ca58c9d89e0022cc35b1c6ce0d
jonavega2006 <ja.alvarezv@uea.edu.ec>
2025-06-08 18:45:42 -0500

    Update encapsulamiento.py

--- encapsulamiento.py
+++ encapsulamiento.py
@@ -1,13 +1,23 @@
-from vehiculo import Vehiculo
+# ENCAPSULAMIENTO: Atributos protegidos y control de acceso
 
-class Bicicleta(Vehiculo):
-    def __init__(self, marca, modelo, tipo_freno):
-        super().__init__(marca, modelo)
-        self.__tipo_freno = tipo_freno  # privado
+class Hechicero:
+    def __init__(self, nombre):
+        self.__nombre = nombre     # Atributo privado
+        self.__mana = 100
 
-    def mostrar_freno(self):
-        print(f"La bicicleta tiene frenos de tipo: {self.__tipo_freno}")
+    def lanzar_hechizo(self, cantidad):
+        if cantidad <= self.__mana:
+            self.__mana -= cantidad
+            print(f"{self.__nombre} lanza un hechizo de {cantidad} puntos de maná")
+        else:
+            print("¡No hay suficiente maná!")
 
-    # Override transportar
-    def transportar(self):
-        print(f"La bicicleta {self.marca} {self.modelo} transporta a una persona.")
+    def ver_mana(self):
+        print(f"{self.__nombre} tiene {self.__mana} de maná")
+
+
+# Uso del encapsulamiento
+merlin = Hechicero("Merlín")
+merlin.ver_mana()
+merlin.lanzar_hechizo(30)
+merlin.lanzar_hechizo(80)
